c8f8dd138cb01caa3e104f576bad3fd5bf56abe7
kpsantos <kpsantos@student.apc.edu.ph>
2025-11-19 21:56:00 +0800

    initialize commit

--- main/parsee_project/settings.py
+++ main/parsee_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)fpgidk%zg+f1jhp+x_7%+tz$r%%^&5es_9s8xq*jr65til6s1'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"] #up to you what you wanna do with this
 
 
 # Application definition
